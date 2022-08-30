de3b7e5061e8818555ddda76a671b4f8c039ba98
Sergio <sergio.troncoso@noirlab.edu>
2022-08-30 18:33:23 -0400

    Add subscriber files

---
+++ queries/queries.py
@@ -0,0 +1,64 @@
+from gql import gql
+
+test_mutation_query = gql('''
+                            query testMutation {
+                              data
+                            }
+                        ''')
+
+test_subscription_query = gql('''
+                            subscription testSubscription {
+                              data
+                             }
+                        ''')
+
+observation_update = gql('''
+                            subscription{
+                              observationEdit{
+                                editType,
+                                value{
+                                   id
+                                },
+                                id
+                              }
+                            }
+                        ''')
+
+program_update = gql('''
+                      subscription{
+                        programEdit{
+                          editType,
+                          value{
+                            id
+                          },
+                          id
+                        }
+                      }
+                    ''')
+
+target_update = gql('''
+                      subscription{
+                        targetEdit{
+                          editType,
+                          value{
+                            id
+                          },
+                          id
+                        }
+                      }
+                    ''')
+
+new_schedule_mutation = gql('''
+                            mutation {
+                              newSchedule(newScheduleInput:{ startTime:"2018-10-01 08:00:00",
+                                                             endTime:"2018-10-03 08:00:00"}){
+                                 __typename
+                                 ... on NewScheduleSuccess{
+                                    success
+                                }
+                                 ... on NewScheduleError{
+                                    error
+                                }
+                              }
+                            }
+                            ''')
